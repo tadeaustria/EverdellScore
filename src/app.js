092058a88b86fd2a6a34256c6fc2afa6c8b04c07
Daniel Gogl <tade@gmx.at>
2024-05-24 17:53:36 +0200

    Ignore card limit for legendaries
    Solves #39

--- src/app.js
+++ src/app.js
@@ -153,7 +153,7 @@ class Application {
 
     addToActivePlayer(cardName) {
         let card = this.findCardByName(cardName);
-        if (card.getOccupiedSpaces(this.activePlayer, true) + this.activePlayer.getOccupiedSpaces() > this.activePlayer.getMaxSpace()) {
+        if (card.rarity != RARITY.legendary && card.getOccupiedSpaces(this.activePlayer, true) + this.activePlayer.getOccupiedSpaces() > this.activePlayer.getMaxSpace()) {
             $("#alert-cardlimit").fadeTo(3000, 500).slideUp(500, function () {
                 $("#alert-cardlimit").slideUp(500);
             });
